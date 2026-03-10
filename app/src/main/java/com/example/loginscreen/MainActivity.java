29824cf6302c12e265a410d899d495d9488cf129
ankitsharma1105 <ankitsharma11052004@gmail.com>
2026-03-11 01:09:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/loginscreen/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.loginscreen;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if (TextUtils.isEmpty(user)) {
+                    username.setError("Enter Username");
+                }
+                else if (TextUtils.isEmpty(pass)) {
+                    password.setError("Enter Password");
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
